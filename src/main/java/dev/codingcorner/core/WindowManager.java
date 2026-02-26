1846aaed9da23a143b310b9ad761326a4662dcec
Dekoder-py <dekod3r.py@gmail.com>
2026-02-27 12:20:13 +1300

    Create a test window!

--- src/main/java/dev/codingcorner/core/WindowManager.java
+++ src/main/java/dev/codingcorner/core/WindowManager.java
@@ -39,7 +39,7 @@ public class WindowManager {
         GLFW.glfwDefaultWindowHints();
         GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_TRUE);
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 4);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 4);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
 
--- src/main/java/dev/codingcorner/test/Launcher.java
+++ src/main/java/dev/codingcorner/test/Launcher.java
@@ -1,9 +1,19 @@
 package dev.codingcorner.test;
 
+import dev.codingcorner.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println(Version.getVersion());
+
+        WindowManager window = new WindowManager("CODING CORNER ENGINE", 800, 400, false);
+        window.init();
+
+        while (!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
     }
 }
